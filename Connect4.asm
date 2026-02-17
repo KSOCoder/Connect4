c3b8c144a0f3f016fb0e6327b6cd17bd2591a08c
Calvin Rutherford <Calvinjay05@gmail.com>
2026-02-17 06:09:01 -0500

    Added calls to player moves and finished board

--- Connect4.asm
+++ Connect4.asm
@@ -51,14 +51,34 @@ print_cell_loop:
 	PUSH     '|'
 	CALL     putchar
 	ADD      ESP,4
+
+	CMP      EBX,1
+	JE       cell_is_1
+	CMP      EBX,2
+	JE       cell_is_2
+	PUSH     OFFSET MSG_CELL_E
+	JMP      print_char
 cell_is_1:
 	PUSH     OFFSET MSG_CELL_1
 	JMP      print_char
-cell_is_2:
+cell_is_2 :
 	PUSH     OFFSET MSG_CELL_2
-print_char:
+print_char :
 	CALL     printf
-	ADD      ESP,4
+	ADD      ESP, 4
 
 	INC      ECX
 	JMP      print_cell_loop
+print_row_end:
+	PUSH     '|'
+	CALL     putchar
+	ADD      ESP,4
+	CALL     print_newline
+
+	INC      EDX
+	JMP      print_row_loop
+print_board_done:
+	PUSH     OFFSET MSG_BORDER
+	CALL     puts
+	ADD      ESP,4
+	RET
