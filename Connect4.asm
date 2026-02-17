e2a7357de47f6625d07612a24acce1a8a8901cc2
Calvin Rutherford <Calvinjay05@gmail.com>
2026-02-17 05:50:45 -0500

    Board and Board Printer

--- Connect4.asm
+++ Connect4.asm
@@ -5,8 +5,28 @@
 ; Board initialization; 42 bytes
 	COLS     EQU 7
 	ROWS     EQU 6
-	BOARD    RESB (ROWS * COLS)
-	MSG_CELL_E  DB  " . ",0
-	MSG_CELL_1  DB  " X ",0
-	MSG_CELL_2  DB  " O ",0
-	COL_NUMS    DB  " 1 2 3 4 5 6 7",0
+	BOARD    RESB(ROWS * COLS)
+	MSG_CELL_E  DB  " . ", 0
+	MSG_CELL_1  DB  " X ", 0
+	MSG_CELL_2  DB  " O ", 0
+	COL_NUMS    DB  " 1 2 3 4 5 6 7", 0
+
+;
+===================================================================
+;
+init_board:
+	MOV      EDI, OFFSET BOARD
+	MOV      ECX, (ROWS* COLS)
+	XOR      EAX, EAX
+	REP      STOSB
+	RET
+
+;
+================================================================== =
+;
+print_board:
+	PUSH     OFFSET COL_NUMS
+	CALL     puts
+	ADD      ESP,4
+	MOV      EDX,0
+	
